b4770bb309ba0a5af62b96938cf6ef64837a7099
Fu-Lin Liu <fulin426@gmail.com>
2018-06-07 20:35:02 -0700

    added config file

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://admin:admin@ds135252.mlab.com:35252/portfolio-tracker';
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 
+'mongodb://admin:admin12@ds151970.mlab.com:51970/react-capstone';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-    'mongodb://admin:admin@ds135252.mlab.com:35252/portfolio-tracker';
+'mongodb://test123:test123@ds241019.mlab.com:41019/restaurants-app-test';
+
 exports.PORT = process.env.PORT || 8080;
